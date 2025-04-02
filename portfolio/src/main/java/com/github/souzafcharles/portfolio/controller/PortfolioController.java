e5b9db8e1be12bbe1edbaca84106ffd0c12b7625
souzafcharles <souzach@outlook.com>
2025-04-02 18:25:45 -0300

    feat: add Portfolio controller class

---
+++ portfolio/src/main/java/com/github/souzafcharles/portfolio/controller/PortfolioController.java
@@ -0,0 +1,32 @@
+package com.github.souzafcharles.portfolio.controller;
+
+import com.github.souzafcharles.portfolio.model.Portfolio;
+import com.github.souzafcharles.portfolio.service.PortfolioService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/portfolio")
+public class PortfolioController {
+
+    @Autowired
+    private PortfolioService service;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @PostMapping
+    public String create(@RequestBody Portfolio portfolio) {
+        Portfolio created = service.create(portfolio);
+        rabbitTemplate.convertAndSend(created);
+        return "Portfolio saved and sent for processing: " + portfolio.getTitle();
+    }
+
+    @GetMapping
+    public List<Portfolio> readAll(){
+        return service.readAll();
+    }
+}
